1929b53d1cbe0fd38bab8c152858e20c59bf5a30
ThroneStack <thronestack@users.noreply.github.com>
2026-08-27 18:58:39 -0400

    Open founding edition checkout

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 window.THRONESTACK_CONFIG = {
-  checkoutUrl: "",
+  checkoutUrl: "https://thronestack.lemonsqueezy.com/checkout/buy/30338763-631e-4440-9427-afbc7dbd08f7",
   launchDate: "August 28, 2026",
-  salesOpen: false
+  salesOpen: true
 };
